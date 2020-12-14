caecef8114842ed2a0029fbf360f195bdb106367
Patryk Tomaszewski <62951604+piciu0908@users.noreply.github.com>
2020-12-14 20:30:25 +0100

    test

--- wms-system-project/Program.cs
+++ wms-system-project/Program.cs
@@ -18,6 +18,7 @@ namespace wms_system_project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("siemano");
         }
     }
 }
